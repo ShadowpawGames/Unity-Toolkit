4440cb795abe0d8849a45cbcf18b088139eeb4f9
Ketchup <krechevskoy@gmail.com>
2024-07-26 13:49:42 -0500

    Update ConfigInfoAttribute.cs
    Changed set for init and sealed the ConfigInfo Attribute.

--- Runtime/Alchemy/Configs/ConfigInfoAttribute.cs
+++ Runtime/Alchemy/Configs/ConfigInfoAttribute.cs
@@ -5,21 +5,21 @@ namespace Shadowpaw.Alchemy {
   /// Provides additional information about a <see cref="Config"/>  object
   /// </summary>
   [AttributeUsage(AttributeTargets.Class)]
-  public class ConfigInfoAttribute : Attribute {
+  public sealed class ConfigInfoAttribute : Attribute {
     /// <summary>
     /// If provided, this will be the display name used in the inspector.
     /// </summary>
-    public string DisplayName { get; set; }
+    public string DisplayName { get; init; }
 
     /// <summary>
     /// If provided, a help button will be displayed in the inspector that links to this URL.
     /// </summary>
-    public string HelpURL { get; set; }
+    public string HelpURL { get; init; }
 
     /// <summary>
     /// If true, this config will hidden from Project Settings
     /// </summary>
-    public bool Hidden { get; set; }
+    public bool Hidden { get; init; }
 
     public ConfigInfoAttribute(string displayName = null) {
       DisplayName = displayName;
